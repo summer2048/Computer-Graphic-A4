34880236141674d3490fd23cfb469b2e8825f89e
KaichengXu007 <59582632+KaichengXu007@users.noreply.github.com>
2021-12-06 14:06:26 -0500

    Update drawing.h

--- drawing.h
+++ drawing.h
@@ -64,3 +64,38 @@ glutSolidCube(GLdouble size)
 {
   drawBox(size, GL_QUADS);
 }
+
+void APIENTRY
+glutSolidTetrahedron1()
+{
+    glBegin(GL_TRIANGLES);
+       // front
+       glTexCoord2d(0.5, 0.0);
+       glVertex3d( 0.0, 2.0,  0.0);
+       glTexCoord2d(0.0, 0.0); 
+       glVertex3d(-1.0, 0.0,  1.0);
+       glTexCoord2d(0.0, 1.0); 
+       glVertex3d( 1.0, 0.0,  1.0);
+       // right
+       glTexCoord2d(0.5, 0.0); 
+       glVertex3d( 0.0, 2.0,  0.0);
+       glTexCoord2d(0.0, 1.0); 
+       glVertex3d( 1.0, 0.0,  1.0);
+       glTexCoord2d(0.5, 1.0); 
+       glVertex3d( 0.0, 0.0, -1.0);
+       // left
+       glTexCoord2d(0.5, 0.0);
+       glVertex3d( 0.0, 2.0,  0.0);
+       glTexCoord2d(0.5, 1.0);
+       glVertex3d( 0.0, 0.0, -1.0);
+       glTexCoord2d(1.0, 0.0);
+       glVertex3d(-1.0, 0.0,  1.0);
+       // bottom
+       glTexCoord2d(1.0, 1.0);
+       glVertex3d( 1.0, 0.0,  1.0);
+       glTexCoord2d(1.0, 0.0);
+       glVertex3d(-1.0, 0.0,  1.0);
+       glTexCoord2d(0.5, 1.0);
+       glVertex3d( 0.0, 0.0, -1.0);
+  glEnd();
+}
